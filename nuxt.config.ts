9404f29345999673a985a0da9a919f0240657b7e
rusj88 <brujacc@mail.ru>
2025-09-15 13:39:07 +0300

    chore: initial project setup

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  srcDir: 'app',
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/fonts', 'nuxt-svgo'],
+
+  css: ['@/assets/styles/main.scss'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/variables" as *;',
+        },
+      },
+    },
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE,
+    },
+  },
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: `${process.env.NUXT_APP_BASE_URL || '/'}favicon.ico`,
+        },
+      ],
+    },
+  },
+})
