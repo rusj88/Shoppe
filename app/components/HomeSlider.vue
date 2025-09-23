f5fb0ce4aac070a654208d80da2f1e24a72e71f9
rusj88 <brujacc@mail.ru>
2025-09-23 16:44:30 +0300

    feat: add BaseSlider

---
+++ app/components/HomeSlider.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  import BaseSlider from '@/components/BaseSlider.vue'
+  import { Pagination, Autoplay, Virtual } from 'swiper/modules'
+  import type { SwiperModule } from 'swiper/types'
+  import type { MySwiperOptions } from '@/types'
+  import SlideOverlay from './SlideOverlay.vue'
+
+  const slides = Array.from(
+    { length: 10 },
+    (_, i) => `https://picsum.photos/600/900?random=${i + 1}`,
+  )
+
+  const settings: MySwiperOptions = {
+    modules: [Pagination, Autoplay, Virtual] as SwiperModule[],
+    loop: true,
+    virtual: true,
+    slidesPerView: 1,
+    spaceBetween: 12,
+    autoplay: { delay: 2500, pauseOnMouseEnter: true },
+    pagination: { clickable: true },
+  }
+</script>
+
+<template>
+  <div class="slider-container">
+    <BaseSlider :slides="slides" :settings="settings">
+      <SlideOverlay />
+    </BaseSlider>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .slider-container {
+    margin-top: 16px;
+  }
+</style>
